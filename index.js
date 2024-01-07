05c6e6100e6ac18b5efba41cfd9020d6fc21ac61
aminabth <126773405+aminabth@users.noreply.github.com>
2024-01-07 23:10:31 +0100

    connectie database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
@@ -6,6 +7,28 @@ app.get('/', (req, res) => {
   res.send('Hallo Wereld!');
 });
 
-app.listen(port, () => {
-  console.log(`Server luistert op poort ${port}`);
-});
+  // Maak een verbinding met de database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'myuser', 
+    password: 'myuser123', 
+    database: 'mijn_evenementen', 
+  });
+  
+  // Verbind met de database
+  db.connect((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Verbonden met de database');
+  });
+  
+  // Middleware voor het verwerken van JSON en urlencoded formuliergegevens
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.listen(port, () => {
+    console.log(`Server luistert op poort ${port}`);
+  });
+
+
